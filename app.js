02cd2611698116eb9bd18e2b50c200504782d40a
Villera69 <william.ramsfeldt@gmail.com>
2022-02-11 15:00:02 +0100

    Basic start to lvl1 MH calculator

---
+++ app.js
@@ -0,0 +1,17 @@
+
+
+let Lvl1MHInput = document.querySelector(".lvl1MHInput")
+
+Lvl1MHInput.addEventListener("submit", handleLvl1MHInput)
+
+function handleLvl1MHInput(event) {
+    event.preventDefault();
+
+    let EnergyPerHitLevel1 = document.getElementsByName("EnergyPerHitLevel1");
+
+    let HitsPerLevel1 = document.getElementsByName("HitsPerLevel1");
+
+    
+    
+}
+
