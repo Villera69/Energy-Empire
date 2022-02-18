eecb1af77010f53173b311376ed86a55e74c6f12
Villera69 <william.ramsfeldt@gmail.com>
2022-02-18 10:42:22 +0100

    Gjorde lite på klickfunktionaliteten. Lägger nu till energy och updaterar storleken

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
+let energy = 0;
+let windGeneretors = 0;
 
 
-let Lvl1MHInput = document.querySelector(".lvl1MHInput")
+const EnergyTotal = document.querySelector(".EnergyTotal")
+const Inventory = document.querySelector(".Inventory")
 
-Lvl1MHInput.addEventListener("submit", handleLvl1MHInput)
 
-function handleLvl1MHInput(event) {
-    event.preventDefault();
+const ClickTarget = document.querySelector(".ClickTarget")
+ClickTarget.addEventListener("click", ClickEnergy)
 
-    let EnergyPerHitLevel1 = document.getElementsByName("EnergyPerHitLevel1");
+updateStats();
 
-    let HitsPerLevel1 = document.getElementsByName("HitsPerLevel1");
 
-    
-    
+
+function ClickEnergy(event) {
+    energy ++;
+    ClickTarget.style.width = '175px';
+    ClickTarget.style.height = '174px';
+    setTimeout(updateStats, 50)
+}
+
+
+function updateStats(){
+
+    EnergyTotal.innerHTML = energy;
+    ClickTarget.style.width = '150px';
+    ClickTarget.style.height = '149px';
 }
 
